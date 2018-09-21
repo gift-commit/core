20e0d0264a742a84de1b5142a09710093f458d52
Brett Spradling <brett@spradling.me>
2018-09-20 21:58:10 -0700

    Removing Unneccessary Docker Build (#12)

--- .build/push_to_ecr.sh
+++ .build/push_to_ecr.sh
@@ -3,6 +3,5 @@
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 eval $(aws ecr get-login --region us-east-1)
-docker build -t gift-commit/core .build/
 docker tag gift-commit/core:latest 046006883301.dkr.ecr.us-east-1.amazonaws.com/gift-commit/core:latest
 docker push 046006883301.dkr.ecr.us-east-1.amazonaws.com/gift-commit/core:latest
