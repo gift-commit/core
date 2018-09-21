74688447ffb59e1118240e1a4acb9c07037158b9
Brett Spradling <bspradling@godaddy.com>
2018-09-20 22:04:51 -0700

    Fixing Amazon ECR URL

--- .build/push_to_ecr.sh
+++ .build/push_to_ecr.sh
@@ -3,5 +3,5 @@
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 eval $(aws ecr get-login --region us-east-1)
-docker tag gift-commit/core:latest 046006883301.dkr.ecr.us-east-1.amazonaws.com/gift-commit/core:latest
-docker push 046006883301.dkr.ecr.us-east-1.amazonaws.com/gift-commit/core:latest
+docker tag gift-commit/core:latest 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
+docker push 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
