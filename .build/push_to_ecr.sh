5878e256a235863dc38ae97737f143a505834c1e
Brett Spradling <brett@spradling.me>
2018-09-20 19:48:23 -0700

    Push Image to Amazon ECR (#10)

---
+++ .build/push_to_ecr.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+eval $(aws ecr get-login --region us-east-1)
+docker build -t gift-commit/core .
+docker tag gift-commit/core:latest 046006883301.dkr.ecr.us-east-1.amazonaws.com/gift-commit/core:latest
+docker push 046006883301.dkr.ecr.us-east-1.amazonaws.com/gift-commit/core:latest
