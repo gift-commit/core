a0944b7cd827ad3cf2b4a4ca30a7514c16f67c75
Brett Spradling <brett@spradling.me>
2018-09-20 23:37:55 -0700

    Fixing Aws Get Login Region (#14)

--- .build/push_to_ecr.sh
+++ .build/push_to_ecr.sh
@@ -2,6 +2,6 @@
 
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
-eval $(aws ecr get-login --region us-east-1)
+eval $(aws ecr get-login --region us-west-2)
 docker tag gift-commit/core:latest 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
 docker push 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
