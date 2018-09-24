bb36368b02bbc96339b4308dc0062a5c2df38138
Brett Spradling <brett@spradling.me>
2018-09-24 00:08:45 -0700

    Versioning Api (#21)

--- .build/deploy-aws.sh
+++ .build/deploy-aws.sh
@@ -1,6 +1,12 @@
-ssh -i "~/.ssh/shared.pem" ec2-user@ec2-35-163-156-14.us-west-2.compute.amazonaws.com << EOF
-   eval $(aws ecr get-login --region us-west-2 --no-include-email)
-   docker pull 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
-   docker stop core && docker rm core
-   docker run --restart=always --name core -p 8080:8080 -d 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+eval $(aws ecr get-login --region us-west-2 --no-include-email)
+docker tag gift-commit/core:latest 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:$TRAVIS_TAG
+docker push 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:$TRAVIS_TAG
+
+ssh -i .build/secrets/decrypted/shared.pem ec2-user@ec2-35-163-156-14.us-west-2.compute.amazonaws.com << EOF
+  eval $(aws ecr get-login --region us-west-2 --no-include-email)
+  docker pull 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
+  docker stop core && docker rm core
+  docker run --restart=always --name core -p 8080:8080 -d 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:$TRAVIS_TAG
 EOF
