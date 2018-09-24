24b28b64719dfe0914d44fc764f8e6863c35d8d4
Brett Spradling <bspradling@godaddy.com>
2018-09-23 22:41:15 -0700

    Versioning Api

--- .build/push_to_ecr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-pip install --user awscli
-export PATH=$PATH:$HOME/.local/bin
-eval $(aws ecr get-login --region us-west-2 --no-include-email)
-docker tag gift-commit/core:latest 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
-docker push 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
