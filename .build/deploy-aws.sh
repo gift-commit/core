5965512893695c341f3aff62a6689d13b5a9e7cc
Brett Spradling <brett@spradling.me>
2018-09-24 08:01:35 -0700

    Adding EC2 as Known Host (#25)

--- .build/deploy-aws.sh
+++ .build/deploy-aws.sh
@@ -4,7 +4,7 @@ eval $(aws ecr get-login --region us-west-2 --no-include-email)
 docker tag gift-commit/core:latest 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:$TRAVIS_TAG
 docker push 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:$TRAVIS_TAG
 
-ssh -o -i "secrets/decrypted/shared.pem" ec2-user@ec2-35-163-156-14.us-west-2.compute.amazonaws.com << EOF
+ssh -i "secrets/decrypted/shared.pem" ec2-user@ec2-35-163-156-14.us-west-2.compute.amazonaws.com << EOF
    eval $(aws ecr get-login --region us-west-2 --no-include-email)
    docker pull 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
    docker stop core && docker rm core
