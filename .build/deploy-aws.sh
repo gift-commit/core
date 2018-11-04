4fe95dde543629b7073def6d53b2eb2d983c590c
Brett Spradling <bspradling@godaddy.com>
2018-11-04 15:32:24 -0800

    Exposing Application on Port 80

--- .build/deploy-aws.sh
+++ .build/deploy-aws.sh
@@ -8,5 +8,5 @@ ssh -i .build/secrets/decrypted/shared.pem ec2-user@ec2-35-163-156-14.us-west-2.
   eval $(aws ecr get-login --region us-west-2 --no-include-email)
   docker pull 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:latest
   docker stop core && docker rm core
-  docker run --restart=always --name core -p 8080:8080 -d 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:$TRAVIS_TAG
+  docker run --restart=always --name core -p 80:8080 -d 046006883301.dkr.ecr.us-west-2.amazonaws.com/gift-commit/core:$TRAVIS_TAG
 EOF
